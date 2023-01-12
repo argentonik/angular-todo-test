5b42ebd395d85082da30b75e974514f2c60edd5e
Volodymyr Lazarchuk <vladimir.lazarchuk@computools.com>
2023-01-13 00:05:42 +0200

    todo service has been added

---
+++ tech-test/src/app/todo/services/toso.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Todo } from '../models/todo.interface';
+import { Observable } from 'rxjs';
+
+export class TodoService {
+  private readonly API_URL = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  public getAll(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.API_URL}/tasks`);
+  }
+
+  public create(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(`${this.API_URL}/tasks`, todo);
+  }
+
+  public update(id: number, todo: Partial<Todo>): Observable<Todo> {
+    return this.http.put<Todo>(`${this.API_URL}/tasks/${id}`, todo);
+  }
+
+  public delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.API_URL}/tasks/${id}`);
+  }
+}
