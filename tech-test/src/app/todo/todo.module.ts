7f10df0c94fd17b2de6dfa1879d3c55b09d13801
Volodymyr Lazarchuk <vladimir.lazarchuk@computools.com>
2023-01-13 00:41:28 +0200

    todo store has been added

--- tech-test/src/app/todo/todo.module.ts
+++ tech-test/src/app/todo/todo.module.ts
@@ -4,10 +4,21 @@ import { CommonModule } from '@angular/common';
 import { TodoRoutingModule } from './todo-routing.module';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoService } from './services/toso.service';
+import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { todoReducer } from './store/todo.reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { TodoEffects } from './store/todos.effects';
 
 @NgModule({
   declarations: [TodoListComponent],
-  imports: [CommonModule, TodoRoutingModule],
+  imports: [
+    CommonModule,
+    TodoRoutingModule,
+    FormsModule,
+    StoreModule.forFeature('todos', todoReducer),
+    EffectsModule.forFeature([TodoEffects]),
+  ],
   providers: [TodoService],
 })
 export class TodoModule {}
