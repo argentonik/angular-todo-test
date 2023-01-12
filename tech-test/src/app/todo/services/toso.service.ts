3294d2e699e94818f21d691e90e917bc65583805
Volodymyr Lazarchuk <vladimir.lazarchuk@computools.com>
2023-01-13 00:06:35 +0200

    todo service has been added

--- tech-test/src/app/todo/services/toso.service.ts
+++ tech-test/src/app/todo/services/toso.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Todo } from '../models/todo.interface';
 import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class TodoService {
   private readonly API_URL = 'http://localhost:3000';
 
--- tech-test/src/app/todo/todo.module.ts
+++ tech-test/src/app/todo/todo.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { TodoRoutingModule } from './todo-routing.module';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoService } from './services/toso.service';
 
 @NgModule({
   declarations: [TodoListComponent],
   imports: [CommonModule, TodoRoutingModule],
+  providers: [TodoService],
 })
 export class TodoModule {}
