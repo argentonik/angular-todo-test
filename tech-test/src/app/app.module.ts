2682170f05bccb66f5a5275579c54c64ea92fb4b
Volodymyr Lazarchuk <vladimir.lazarchuk@computools.com>
2023-01-12 18:29:04 +0200

    ngrx added

--- tech-test/src/app/app.module.ts
+++ tech-test/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [HttpClient],
   bootstrap: [AppComponent]
