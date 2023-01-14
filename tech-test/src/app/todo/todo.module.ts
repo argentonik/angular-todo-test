137ecd78defc591f26e96edaafab28f78fbf0859
Volodymyr Lazarchuk <vladimir.lazarchuk@computools.com>
2023-01-14 14:29:35 +0200

    file was renamed

--- tech-test/src/app/todo/todo.module.ts
+++ tech-test/src/app/todo/todo.module.ts
@@ -8,7 +8,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { todoReducer } from './store/todo.reducers';
 import { EffectsModule } from '@ngrx/effects';
-import { TodoEffects } from './store/todos.effects';
+import { TodoEffects } from './store/todo.effects';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
